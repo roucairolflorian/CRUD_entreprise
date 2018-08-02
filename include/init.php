53cbe331d276ff6d9b601b32e0fdfb3dc92e6f59
flo <roucairol.florian@gmail.com>
2018-08-02 16:56:07 +0200

    ajout formulaire

--- include/init.php
+++ include/init.php
@@ -1,7 +1,7 @@
 <?php
 //------ connexion bdd
     $pdo = new PDO('mysql:host=localhost;dbnam=entreprise', 'root', '', array
-        (PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT => 'SET NAMES utf8'));
+        (PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
     //------------ session
     session_start();
